32a16f3e4089ac0a29d068a04e1127e691e4a13b
JuanAller <juan.aller@mercadolibre.com>
2018-07-24 01:35:14 -0300

    ref tests

--- src/api/builder/request_builder_test.go
+++ src/api/builder/request_builder_test.go
@@ -8,9 +8,49 @@ import (
 	"errors"
 )
 
-type checkFunc func(response *Response) error
+type checkRequestFunc func(request *http.Request) error
 
-func checkStatusCode(statusCode int) checkFunc {
+func checkReqHeader(key string, value string) checkRequestFunc {
+	return func(request *http.Request) error {
+		if request.Header.Get(key) != value {
+			return fmt.Errorf("Expected value : %v, in header %v, but got : %v ", value, key, request.Header.Get(key))
+		}
+		return nil
+	}
+}
+
+func checkReqQueryParam(key string, value string) checkRequestFunc {
+	return func(request *http.Request) error {
+		if request.URL.Query().Get(key) != value {
+			return fmt.Errorf("Expected value : %v, in query param : %v, but got : %v ", value, key, request.URL.Query().Get(key))
+		}
+		return nil
+	}
+}
+
+func checkReqMethod(method string) checkRequestFunc {
+	return func(request *http.Request) error {
+		if request.Method != method {
+			return fmt.Errorf("Expected method : %v, but got : %v ", method, request.Method)
+		}
+		return nil
+	}
+}
+
+func checkReqFuncs(checks ...checkRequestFunc) checkRequestFunc {
+	return func(request *http.Request) error {
+		for _, check := range checks {
+			if err := check(request); err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+}
+
+type checkRespFunc func(response *Response) error
+
+func checkStatusCode(statusCode int) checkRespFunc {
 	return func(response *Response) error {
 		if statusCode != response.StatusCode {
 			return fmt.Errorf("Expected : %v , but got : %v ", statusCode, response.StatusCode)
@@ -19,7 +59,7 @@ func checkStatusCode(statusCode int) checkFunc {
 	}
 }
 
-func checkNotError() checkFunc {
+func checkNotError() checkRespFunc {
 	return func(response *Response) error {
 		if response.Error != nil {
 			return fmt.Errorf("Not expected error : %v ", response.Error)
@@ -28,7 +68,7 @@ func checkNotError() checkFunc {
 	}
 }
 
-func checkErrorMessage(errMessage string) checkFunc {
+func checkErrorMessage(errMessage string) checkRespFunc {
 	return func(response *Response) error {
 		if errMessage != response.Error.Error() {
 			fmt.Errorf("Expected error message : %v, but got : %v ", errMessage, response.Error.Error())
@@ -37,7 +77,7 @@ func checkErrorMessage(errMessage string) checkFunc {
 	}
 }
 
-func checkFuncs(checks ...checkFunc) checkFunc {
+func checkRespFuncs(checks ...checkRespFunc) checkRespFunc {
 	return func(response *Response) error {
 		for _, check := range checks {
 			if err := check(response); err != nil {
@@ -52,11 +92,17 @@ func TestGet(t *testing.T) {
 	responseMap := make(map[string]string)
 	response := Get(&mock.HttpClientMock{
 		MakeResponseFunction: func(request *http.Request) (*http.Response, error) {
+			if err := checkReqFuncs(checkReqMethod("GET"))(request); err != nil {
+				return nil, err
+			}
 			return mock.NewJsonResponse(http.StatusOK, map[string]string{"name": "aName"})
 		},
-	}, "http://test/get_ok").Execute(&responseMap)
+	}, "http://test/get_ok").
+		LogRequestBody().
+		LogResponseBody().
+		Execute(&responseMap)
 
-	if err := checkFuncs(checkNotError())(response); err != nil {
+	if err := checkRespFuncs(checkNotError())(response); err != nil {
 		t.Error(err)
 	}
 
@@ -69,11 +115,17 @@ func TestGetNotFound(t *testing.T) {
 	responseMap := make(map[string]string)
 	response := Get(&mock.HttpClientMock{
 		MakeResponseFunction: func(request *http.Request) (*http.Response, error) {
+			if err := checkReqFuncs(checkReqMethod("GET"))(request); err != nil {
+				return nil, err
+			}
 			return mock.NewJsonResponse(http.StatusNotFound, map[string]string{"status": "not_found"})
 		},
-	}, "http://test/get_not_found").Execute(&responseMap)
+	}, "http://test/get_not_found").
+		LogRequestBody().
+		LogResponseBody().
+		Execute(&responseMap)
 
-	if err := checkFuncs(checkStatusCode(http.StatusNotFound), checkNotError())(response); err != nil {
+	if err := checkRespFuncs(checkStatusCode(http.StatusNotFound), checkNotError())(response); err != nil {
 		t.Error(err)
 	}
 }
@@ -82,11 +134,17 @@ func TestGetWithError(t *testing.T) {
 	responseMap := make(map[string]string)
 	response := Get(&mock.HttpClientMock{
 		MakeResponseFunction: func(request *http.Request) (*http.Response, error) {
+			if err := checkReqFuncs(checkReqMethod("GET"))(request); err != nil {
+				return nil, err
+			}
 			return nil, errors.New("an error")
 		},
-	}, "http://test/get_not_found").Execute(&responseMap)
+	}, "http://test/get_not_found").
+		LogRequestBody().
+		LogResponseBody().
+		Execute(&responseMap)
 
-	if err := checkFuncs(checkErrorMessage("an error"))(response); err != nil {
+	if err := checkRespFuncs(checkErrorMessage("an error"))(response); err != nil {
 		t.Error(err)
 	}
 }
@@ -95,11 +153,17 @@ func TestGetServerError(t *testing.T) {
 	responseMap := make(map[string]string)
 	response := Get(&mock.HttpClientMock{
 		MakeResponseFunction: func(request *http.Request) (*http.Response, error) {
+			if err := checkReqFuncs(checkReqMethod("GET"))(request); err != nil {
+				return nil, err
+			}
 			return mock.NewJsonResponse(http.StatusInternalServerError, map[string]string{"status": "internal_server_error"})
 		},
-	}, "http://test/get_server_error").Execute(&responseMap)
+	}, "http://test/get_server_error").
+		LogRequestBody().
+		LogResponseBody().
+		Execute(&responseMap)
 
-	if err := checkFuncs(checkStatusCode(http.StatusInternalServerError), checkNotError())(response); err != nil {
+	if err := checkRespFuncs(checkStatusCode(http.StatusInternalServerError), checkNotError())(response); err != nil {
 		t.Error(err)
 	}
 }
@@ -108,16 +172,19 @@ func TestGetWithContentType(t *testing.T) {
 	responseMap := make(map[string]interface{})
 	response := Get(&mock.HttpClientMock{
 		MakeResponseFunction: func(request *http.Request) (*http.Response, error) {
-			if request.Header.Get("Content-Type") != APPLICATIONJSON {
-				return mock.NewJsonResponse(http.StatusInternalServerError, map[string]string{"status": "fail"})
+			if err := checkReqFuncs(checkReqMethod("GET"),
+				checkReqHeader("Content-Type", "application/json"))(request); err != nil {
+				return nil, err
 			}
 			return mock.NewJsonResponse(http.StatusOK, map[string]string{"status": "status_ok"})
 		},
 	}, "http://test/get_with_content_type").
 		WithJSONContentType().
+		LogRequestBody().
+		LogResponseBody().
 		Execute(&responseMap)
 
-	if err := checkFuncs(checkStatusCode(http.StatusOK), checkNotError())(response); err != nil {
+	if err := checkRespFuncs(checkStatusCode(http.StatusOK), checkNotError())(response); err != nil {
 		t.Error(err)
 	}
 }
@@ -126,11 +193,9 @@ func TestGetXML(t *testing.T) {
 	responseMap := &Response{}
 	response := Get(&mock.HttpClientMock{
 		MakeResponseFunction: func(request *http.Request) (*http.Response, error) {
-			if request.Header.Get("Content-Type") != APPLICATIONXML {
-				return mock.NewXmlResponse(http.StatusInternalServerError, &Response{
-					StatusCode: http.StatusInternalServerError,
-					Error:      nil,
-				})
+			if err := checkReqFuncs(checkReqMethod("GET"),
+				checkReqHeader("Content-Type", "application/xml"))(request); err != nil {
+				return nil, err
 			}
 			return mock.NewXmlResponse(http.StatusOK, &Response{
 				StatusCode: http.StatusOK,
@@ -139,9 +204,11 @@ func TestGetXML(t *testing.T) {
 		},
 	}, "http://test/get_with_xml").
 		WithXMLContentType().
+		LogRequestBody().
+		LogResponseBody().
 		Execute(responseMap)
 
-	if err := checkFuncs(checkStatusCode(http.StatusOK), checkNotError())(response); err != nil {
+	if err := checkRespFuncs(checkStatusCode(http.StatusOK), checkNotError())(response); err != nil {
 		t.Error(err)
 	}
 
@@ -154,16 +221,19 @@ func TestRequestBuilder_WithQueryParam(t *testing.T) {
 	responseMap := make(map[string]interface{})
 	response := Get(&mock.HttpClientMock{
 		MakeResponseFunction: func(request *http.Request) (*http.Response, error) {
-			if request.URL.Query().Get("query_param") != "my_param" {
-				mock.NewJsonResponse(http.StatusInternalServerError, map[string]string{"status": "error"})
+			if err := checkReqFuncs(checkReqMethod("GET"),
+				checkReqQueryParam("query_param", "my_param"))(request); err != nil {
+				return nil, err
 			}
 			return mock.NewJsonResponse(http.StatusOK, map[string]string{"status": "status_ok"})
 		},
 	}, "http://test/get_with_query_param").
 		WithQueryParam("query_param", "my_param").
+		LogRequestBody().
+		LogResponseBody().
 		Execute(&responseMap)
 
-	if err := checkFuncs(checkStatusCode(http.StatusOK), checkNotError())(response); err != nil {
+	if err := checkRespFuncs(checkStatusCode(http.StatusOK), checkNotError())(response); err != nil {
 		t.Error(err)
 	}
 }
@@ -172,13 +242,18 @@ func TestPostOk(t *testing.T) {
 	responseMap := make(map[string]string)
 	response := Post(&mock.HttpClientMock{
 		MakeResponseFunction: func(request *http.Request) (*http.Response, error) {
+			if err := checkReqFuncs(checkReqMethod("POST"))(request); err != nil {
+				return nil, err
+			}
 			return mock.NewJsonResponse(http.StatusCreated, map[string]string{"my_field": "my_value"})
 		},
 	}, "http://test/post_ok").
 		WithBody(map[string]string{"my_field": "my_value"}).
+		LogRequestBody().
+		LogResponseBody().
 		Execute(&responseMap)
 
-	if err := checkFuncs(checkStatusCode(http.StatusCreated), checkNotError())(response); err != nil {
+	if err := checkRespFuncs(checkStatusCode(http.StatusCreated), checkNotError())(response); err != nil {
 		t.Error(err)
 	}
 
@@ -191,11 +266,17 @@ func TestPostWithServerError(t *testing.T) {
 	responseMap := make(map[string]interface{})
 	response := Post(&mock.HttpClientMock{
 		MakeResponseFunction: func(request *http.Request) (*http.Response, error) {
+			if err := checkReqFuncs(checkReqMethod("POST"))(request); err != nil {
+				return nil, err
+			}
 			return mock.NewJsonResponse(http.StatusInternalServerError, map[string]string{"status": "error"})
 		},
-	}, "http://test/post_server_error").Execute(&responseMap)
+	}, "http://test/post_server_error").
+		LogRequestBody().
+		LogResponseBody().
+		Execute(&responseMap)
 
-	if err := checkFuncs(checkStatusCode(http.StatusInternalServerError), checkNotError())(response); err != nil {
+	if err := checkRespFuncs(checkStatusCode(http.StatusInternalServerError), checkNotError())(response); err != nil {
 		t.Error(err)
 	}
 }
@@ -204,17 +285,20 @@ func TestGetWithBasicAuthentication(t *testing.T) {
 	responseMap := make(map[string]interface{})
 	response := Get(&mock.HttpClientMock{
 		MakeResponseFunction: func(request *http.Request) (*http.Response, error) {
-			if request.Header.Get("Authorization") != "Basic YWRtaW46YWRtaW4=" {
-				return mock.NewJsonResponse(http.StatusInternalServerError, map[string]string{"status": "fail"})
+			if err := checkReqFuncs(checkReqMethod("GET"),
+				checkReqHeader("Authorization", "Basic YWRtaW46YWRtaW4="))(request); err != nil {
+				return nil, err
 			}
 			return mock.NewJsonResponse(http.StatusOK, map[string]string{"status": "ok"})
 		},
 	}, "http://test/get_basic_auth").
 		WithJSONContentType().
+		LogRequestBody().
+		LogResponseBody().
 		WithBasicAuthorization("admin", "admin").
 		Execute(&responseMap)
 
-	if err := checkFuncs(checkStatusCode(http.StatusOK), checkNotError())(response); err != nil {
+	if err := checkRespFuncs(checkStatusCode(http.StatusOK), checkNotError())(response); err != nil {
 		t.Error(err)
 	}
 }
@@ -223,8 +307,9 @@ func TestGetWithGZIPCompression(t *testing.T) {
 	responseMap := make(map[string]interface{})
 	response := Get(&mock.HttpClientMock{
 		MakeResponseFunction: func(request *http.Request) (*http.Response, error) {
-			if request.Header.Get("Accept-Encoding") != "gzip" {
-				return mock.NewJsonGzipResponse(http.StatusInternalServerError, map[string]string{"status": "fail"})
+			if err := checkReqFuncs(checkReqMethod("GET"),
+				checkReqHeader("Accept-Encoding", "gzip"))(request); err != nil {
+				return nil, err
 			}
 			return mock.NewJsonGzipResponse(http.StatusOK, map[string]string{"status": "ok"})
 		},
@@ -235,7 +320,7 @@ func TestGetWithGZIPCompression(t *testing.T) {
 		LogResponseBody().
 		Execute(&responseMap)
 
-	if err := checkFuncs(checkStatusCode(http.StatusOK), checkNotError())(response); err != nil {
+	if err := checkRespFuncs(checkStatusCode(http.StatusOK), checkNotError())(response); err != nil {
 		t.Error(err)
 	}
 
